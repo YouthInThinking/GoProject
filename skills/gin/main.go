3ec52bbe321c3e0acf8f0444490bd4a32273bb6b
YouthInThinking <19925469039@163.com>
2025-04-26 15:34:41 +0800

    init gin

---
+++ skills/gin/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// 设置 Gin 模式为发布模式
+	// gin.SetMode(gin.ReleaseMode)
+	server := gin.Default()
+	server.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "ping",
+		})
+	})
+	if err := server.Run(":8080"); err != nil {
+		fmt.Println("Failed to start server:", err.Error())
+		os.Exit(1)
+	} else {
+		fmt.Println("Server started on port 8080")
+
+	}
+}
